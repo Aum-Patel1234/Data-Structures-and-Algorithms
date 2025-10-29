d78dace5fc0fbeba84759f234aa2e2300d4db615
Aum Patel <aumpatel131@gmail.com>
2025-10-29 21:15:57 +0530

    shortest path in DAG using topoSort

--- Graph/include/graph.hpp
+++ Graph/include/graph.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
+#include <climits>
+#include <cstdint>
 #include <stack>
 #ifndef GRAPH_H
 #define GRAPH_H
-#include <utility> // Required for std::pair
+#include <utility>  // Required for std::pair
 #include <vector>
 
 /*
@@ -29,18 +31,15 @@ namespace graph_data {
 const int num_nodes = 6;
 
 // Edge list representation of the graph
-const std::vector<std::pair<int, int>> edges = {{0, 1}, {0, 2}, {1, 2}, {1, 3},
-                                                {1, 4}, {3, 4}, {4, 5}};
-const std::vector<std::pair<int, int>> edges1 = {
-    {0, 1}, {0, 2}, {1, 3}, {2, 4}, {2, 5}};
+const std::vector<std::pair<int, int>> edges = {{0, 1}, {0, 2}, {1, 2}, {1, 3}, {1, 4}, {3, 4}, {4, 5}};
+const std::vector<std::pair<int, int>> edges1 = {{0, 1}, {0, 2}, {1, 3}, {2, 4}, {2, 5}};
 
 const std::vector<std::pair<int, int>> edgesEvenCycle = {
-    {0, 1}, {1, 2}, {2, 3}, {3, 0}, // square cycle (0-1-2-3-0)
-    {2, 4}, {4, 5}                  // chain extension (extra nodes 4 and 5)
+    {0, 1}, {1, 2}, {2, 3}, {3, 0},  // square cycle (0-1-2-3-0)
+    {2, 4}, {4, 5}                   // chain extension (extra nodes 4 and 5)
 };
 
-const std::vector<std::pair<int, int>> edgesDAG = {{0, 1}, {0, 2}, {1, 3},
-                                                   {2, 3}, {3, 4}, {2, 5}};
+const std::vector<std::pair<int, int>> edgesDAG = {{0, 1}, {0, 2}, {1, 3}, {2, 3}, {3, 4}, {2, 5}};
 
 //
 // 0 ----- 1
@@ -48,39 +47,40 @@ const std::vector<std::pair<int, int>> edgesDAG = {{0, 1}, {0, 2}, {1, 3},
 // |       |
 // 3 ----- 2 ---- 4 ---- 5
 const std::vector<std::pair<int, int>> edgesDirectedCycle = {
-    {0, 1}, {1, 2}, {2, 0}, // cycle: 0 -> 1 -> 2 -> 0
-    {2, 3}, {3, 4},         // chain from the cycle
-    {4, 2} // another back edge forming a cycle: 2 -> 3 -> 4 -> 2
+    {0, 1}, {1, 2}, {2, 0},  // cycle: 0 -> 1 -> 2 -> 0
+    {2, 3}, {3, 4},          // chain from the cycle
+    {4, 2}                   // another back edge forming a cycle: 2 -> 3 -> 4 -> 2
 };
 
-} // namespace graph_data
+const std::vector<std::vector<int>> weightedEdges = {{0, 1, 5}, {0, 2, 3}, {1, 3, 6},  {1, 2, 2}, {2, 4, 4},
+                                                     {2, 5, 2}, {2, 3, 7}, {3, 4, -1}, {4, 5, -2}};
+
+}  // namespace graph_data
 
 class Graph {
-private:
-  void dfsHelper(const std::vector<std::vector<int>> &graph,
-                 std::vector<int> &dfs, std::vector<bool> &vis, int i);
-  bool detectCycleHelper(const std::vector<std::vector<int>> &graph,
-                         std::vector<bool> &vis, int curr, int parent);
-  bool isBipartiteDFSHelper(const std::vector<std::vector<int>> &graph,
-                            std::vector<int> &color, int curr,
+ private:
+  void dfsHelper(const std::vector<std::vector<int>>& graph, std::vector<int>& dfs, std::vector<bool>& vis, int i);
+  void dfsShortestPathFromSrcDAGhelper(const std::vector<std::vector<std::pair<int, int>>>& adj,
+                                       std::vector<uint8_t>& vis, std::stack<int>& stk, int curr);
+  bool detectCycleHelper(const std::vector<std::vector<int>>& graph, std::vector<bool>& vis, int curr, int parent);
+  bool isBipartiteDFSHelper(const std::vector<std::vector<int>>& graph, std::vector<int>& color, int curr,
                             int currentColor);
-  bool hasCycleDirectedGraph(const std::vector<std::vector<int>> &graph,
-                             std::vector<bool> &vis, std::vector<bool> &pathVis,
-                             int curr);
-  void topoSortDFS(std::vector<std::vector<int>> &graph, std::vector<bool> &vis,
-                   std::stack<int> &stk, int i);
+  bool hasCycleDirectedGraph(const std::vector<std::vector<int>>& graph, std::vector<bool>& vis,
+                             std::vector<bool>& pathVis, int curr);
+  void topoSortDFS(std::vector<std::vector<int>>& graph, std::vector<bool>& vis, std::stack<int>& stk, int i);
 
-public:
-  std::vector<int> bfs(std::vector<std::vector<int>> &graph);
-  std::vector<int> dfs(std::vector<std::vector<int>> &graph);
-  std::vector<int> topoSort(std::vector<std::vector<int>> &graph);
-  std::vector<int> kahnSort(std::vector<std::vector<int>> &graph);
-  bool detectCycleDirectedGraphKahnAlgo(std::vector<std::vector<int>> &graph);
-  bool detectCycleDFS(std::vector<std::vector<int>> &graph);
-  bool detectCycleBFS(std::vector<std::vector<int>> &graph);
-  bool detectCycleDirectedGraph(std::vector<std::vector<int>> &graph);
-  bool isBipartiteBFS(std::vector<std::vector<int>> &graph);
-  bool isBipartiteDFS(std::vector<std::vector<int>> &graph);
+ public:
+  std::vector<int> bfs(std::vector<std::vector<int>>& graph);
+  std::vector<int> dfs(std::vector<std::vector<int>>& graph);
+  std::vector<int> topoSort(std::vector<std::vector<int>>& graph);
+  std::vector<int> kahnSort(std::vector<std::vector<int>>& graph);
+  std::vector<int> shortestPathFromSrcDAG(int V, int E, const std::vector<std::vector<int>>& edges);
+  bool detectCycleDirectedGraphKahnAlgo(std::vector<std::vector<int>>& graph);
+  bool detectCycleDFS(std::vector<std::vector<int>>& graph);
+  bool detectCycleBFS(std::vector<std::vector<int>>& graph);
+  bool detectCycleDirectedGraph(std::vector<std::vector<int>>& graph);
+  bool isBipartiteBFS(std::vector<std::vector<int>>& graph);
+  bool isBipartiteDFS(std::vector<std::vector<int>>& graph);
 };
 
 #endif
---
+++ Graph/src/shortest_path_dag.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+
+#include "../include/graph.hpp"
+
+void Graph::dfsShortestPathFromSrcDAGhelper(const std::vector<std::vector<std::pair<int, int>>>& adj,
+                                            std::vector<uint8_t>& vis, std::stack<int>& stk, int curr) {
+  vis[curr] = 1;
+
+  for (auto& neighbor : adj[curr]) {
+    if (!vis[neighbor.first]) dfsShortestPathFromSrcDAGhelper(adj, vis, stk, neighbor.first);
+  }
+
+  stk.push(curr);
+}
+
+std::vector<int> Graph::shortestPathFromSrcDAG(int V, int E, const std::vector<std::vector<int>>& graph) {
+  std::vector<std::vector<std::pair<int, int>>> adj(V);
+  for (auto& edge : graph) {
+    adj[edge[0]].push_back({edge[1], edge[2]});
+  }
+
+  std::stack<int> stk;
+  std::vector<uint8_t> vis(V);
+  for (int i = 0; i < V; i++) {
+    if (!vis[i]) dfsShortestPathFromSrcDAGhelper(adj, vis, stk, i);
+  }
+
+  std::vector<int> ans(V, INT_MAX);
+  ans[0] = 0;
+  while (!stk.empty()) {
+    int curr = stk.top();
+    stk.pop();
+
+    if (ans[curr] != INT_MAX) {  // as it leads to int overflow
+      for (auto& neighbor : adj[curr]) {
+        auto& [edge, weight] = neighbor;
+        ans[edge] = std::min(ans[edge], ans[curr] + weight);
+      }
+    }
+  }
+
+  for (int& val : ans)
+    if (val == INT_MAX) val = -1;  // not reachable
+
+  return ans;
+}
+
+int main() {
+  Graph g;
+  auto& edges = graph_data::weightedEdges;
+  const int V = 6, E = edges.size();
+
+  std::vector<int> result = g.shortestPathFromSrcDAG(V, E, edges);
+
+  std::cout << "Shortest path from source 0 to all vertices:\n";
+  for (int i = 0; i < V; ++i) {
+    std::cout << "0 -> " << i << " = " << result[i] << "\n";
+  }
+
+  return 0;
+}
